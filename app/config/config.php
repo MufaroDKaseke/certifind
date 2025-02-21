28d8f7631508904fe28399f145a0881b5fb095a9
MufaroDKaseke <mufarodkaseke@gmail.com>
2025-02-21 19:21:30 +0200

    Setup libraries and tested

---
+++ app/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+// Load Environment Variables
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->load();
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once './vendor/autoload.php';
+require_once './app/config/config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +9,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="./vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= $_ENV['ROOT'] ?>/assets/css/style.css">
 </head>
 
 <body>
 
+  <h1 class="text-primary">Something</h1>
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="./vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
 </body>
 
